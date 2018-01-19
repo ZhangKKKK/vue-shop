186f51706709ec109af3a3bdb7ff2b56dbf898e5
pcfwj <1140982740@qq.com>
2018-01-19 17:57:54 +0800

    1.2

--- src/components/home/banner.vue
+++ src/components/home/banner.vue
@@ -1,29 +1,25 @@
 <template>
   <div class="banner">
-    <mt-swipe :auto="4000">
-      <mt-swipe-item v-for="v in bannerImg"><img :src="v.src" alt=""></mt-swipe-item>
+    <mt-swipe :auto="3000">
+      <mt-swipe-item v-for="v in images"><img :src="v.url"></mt-swipe-item>
     </mt-swipe>
   </div>
-
 </template>
-
 <script>
   export default {
     name: 'banner',
-    props:['bannerImg'],
+    props: ['images'],
     data(){
       return{}
     }
   }
 </script>
-
 <style>
-.banner{
-  width: 100%;
-  height: 2.8rem;
-  background: red;
-}
-  img{
+  .banner{
+    width: 100%;
+    height: 2.8rem;
+  }
+  .banner img{
     width: 100%;
   }
 </style>
